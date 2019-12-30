d0603cc43919da92a23ad1b4ef7b7379357a2815
Akin <`m.akin.unal@gmail.com`>
2019-12-30 14:29:40 +0200

    Adding redux

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import React from 'react';
 import classes from './App.module.css';
 import { NavLink, Route } from 'react-router-dom';
 import Spinner from './Components/UI/Spinner/Spinner';
+import { connect } from 'react-redux';
 
-function App() {
+class App extends React.Component {
   // axios.get('https://my-json-server.typicode.com/jubs16/Products/Products')
-  return (
-    <div className={classes.App}>
-      <NavLink className={classes.links} to='/'>Home</NavLink>
-      <NavLink className={classes.links} to='/about'>About</NavLink>
-      <NavLink className={classes.links} to='/users'>Users</NavLink>
-
-      <Route path='/' exact render={() => <h1>HOME PAGE</h1>}/>
-      <Route path='/about' component={Spinner}/>
-      <Route path='/users' render={() => <h1>USERS PAGE</h1>}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className={classes.App}>
+        <div>
+          <NavLink className={classes.links} to='/'>Home</NavLink>
+          <NavLink className={classes.links} to='/about'>About</NavLink>
+          <NavLink className={classes.links} to='/users'>Users</NavLink>
+        </div>
+        <h1>{this.props.ctr}</h1>
+        <button onClick={this.props.onIncrementCounter}>CLICK ME</button>
+  
+        <Route path='/' exact render={() => <h1>HOME PAGE</h1>}/>
+        <Route path='/about' component={Spinner}/>
+        <Route path='/users' render={() => <h1>USERS PAGE</h1>}/>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
